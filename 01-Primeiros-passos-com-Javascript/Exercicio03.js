b81cadb524bca1aea8b399b7a57113f1b6f693c7
Eugênio Freire <eugenio9008@gmail.com>
2020-07-24 14:16:04 -0300

    initial commit ex03

---
+++ 01-Primeiros-passos-com-Javascript/Exercicio03.js
@@ -0,0 +1,14 @@
+//Dado um vetor de números, como poderia ser realizada a soma de todos os valores ímpares utilizando reduce e filter.
+
+const v = [1, 2, 3, 4, 5]
+
+const somatorio = (valorAnterior, valorAtual, indexAtual, vetor) => {
+  return valorAnterior + valorAtual
+}
+
+const sumImpar = v.filter(function (item) {
+  if (item % 2 !== 0)
+    return item
+}).reduce(somatorio)
+
+console.log(sumImpar)
