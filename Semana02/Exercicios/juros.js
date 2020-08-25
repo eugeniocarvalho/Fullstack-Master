708ca425f09bfac039c28eb27542a788b1be0d05
Eugênio Carvalho <eugenio9008@gmail.com>
2020-08-25 17:31:27 -0300

    initial commit

---
+++ Semana02/Exercicios/juros.js
@@ -0,0 +1,27 @@
+'Crie um módulo juros.js, e exporte dele uma função jurosSimples que recebe C (capital), i (juros em decimal. ex: 3,5% = 0.035) e t (tempo). E retorne o juros simples do período (C * i * t ).'
+
+const jurosSimples = (c, i, t) => c * i * t
+
+'Crie uma nova função que dado as mesmas variáveis do exercício anterior, retorne o montante total C + juros simples.'
+
+const montanteJurosSimples = ({jurosSimples})=> (c, i, t) => c + jurosSimples(c, i, t) 
+
+'Crie uma função montanteJurosCompostos que recebe C (capital), i (juros em decimal) e t (tempo) e retorno o montante para o período, dado pela fórmula: M = C * (1 +  i) ^ n​.'
+
+const montanteJurosCompostos = (C, i, t) => C * Math.pow((1 + i), t)
+
+'O exercício anterior já retorna o montante (capital + juros). Crie uma função em juros.js que retorne somente os juros.'
+
+const jurosComposto = ({ montanteJurosCompostos }) => (C, i, t) => montanteJurosCompostos(C, i, t) - C
+
+
+module.exports = {
+    jurosSimples,
+    montanteJurosSimples: montanteJurosSimples({jurosSimples}),
+    montanteJurosCompostos,
+    jurosComposto: jurosComposto({montanteJurosCompostos}),
+    pure: {
+        montanteJurosSimples,
+        jurosComposto
+    }
+}
---
+++ Semana02/Exercicios/juros.test.js
@@ -0,0 +1,65 @@
+const juros = require('./juros')
+const { montanteJurosSimples } = require('./juros')
+
+test('jurosSimples', () => {
+    const C = 100
+    const i = 0.10
+    const t = 1
+    const jurosEsperado = 10
+    const jurosCalculo = juros.jurosSimples(C, i, t)
+    expect(jurosCalculo).toBe(jurosEsperado)
+})
+
+/*
+//Falhando de proposito
+test('jurosSimples', () => {
+    const C = 100
+    const i = 0.10
+    const t = 1
+    const jurosEsperado = 15
+    const jurosCalculo = juros.jurosSimples(C, i, t)
+    expect(jurosCalculo).toBe(jurosEsperado)
+})*/
+
+test('JurosMontateSimples', () => {
+  const C = 100
+  const i = 0.10
+  const t = 1
+  
+  const montanteEsperado = 110
+  const jurosSimples = jest.fn()
+
+  jurosSimples.mockImplementation(() => 10)
+
+  const montanteSimples = juros.pure.montanteJurosSimples({jurosSimples})
+  const montante = montanteSimples(C, i, t)
+  
+  expect(jurosSimples.mock.calls[0]).toEqual([C, i, t])
+  expect(montante).toBe(montanteEsperado)
+})
+
+test('jurosMonstanteComposto', () =>{
+    const C = 1000
+    const i = 0.10
+    const t = 1
+    const jurosCompostoEsperado = 1100
+    const jurosCompostoCalculado = juros.montanteJurosCompostos(C, i, t)
+
+    expect(jurosCompostoCalculado).toBe(jurosCompostoEsperado)
+})
+
+test('jurosComposto', () =>{
+    const C = 1000
+    const i = 0.10
+    const t = 1
+    const jurosCompostoEsperado = 100
+    const montanteJurosCompostos = jest.fn()
+
+    montanteJurosCompostos.mockImplementation(() => 1100)
+    montanteComposto = juros.pure.jurosComposto({ montanteJurosCompostos })
+    
+    const juroComposto = montanteComposto(C, i, t)
+
+    expect(montanteJurosCompostos.mock.calls[0]).toEqual([C, i, t])
+    expect(juroComposto).toBe(jurosCompostoEsperado)
+})
